2c2e13e42f79009dbfc3188f94fc239823d4d13a
azakg <azakgzuu@gmail.com>
2025-05-02 16:04:44 -0400

    Post1

--- profiles-rest-api/profiles_api/views.py
+++ profiles-rest-api/profiles_api/views.py
@@ -1,8 +1,13 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from rest_framework import serializers
 
 class HelloApiView(APIView):
     """Test API View"""
+    serializers_class = serializers.HelloSerializer
+
     def get(self, request, format=None):
         """Returns a list of APIView features"""
         an_apivire = [
@@ -13,3 +18,17 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message':'Hello!', 'an_apiview': an_apivire})
+
+    def post(self, request):
+        """Create a hello message with our name"""
+        serializer = self.serializers_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
